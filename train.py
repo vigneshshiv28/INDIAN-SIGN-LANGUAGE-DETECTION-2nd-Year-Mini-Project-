27d306db6b935b2b092741f65bbcb9f1d3b0ba77
Vignesh Shivhare <vigneshshivhare28@gmail.com>
2023-02-25 18:17:09 +0530

    model trained and added

---
+++ train.py
@@ -0,0 +1,42 @@
+from image_func import *
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.callbacks import TensorBoard
+label_map = {label:num for num, label in enumerate(signs)}
+# print(label_map)
+Processed_Path = "D:\isl\Processed_Data"
+sequences, labels = [], []
+for action in signs:
+    for sequence in range(999,1200):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(Processed_Path, action,  "{}.npy".format(sequence)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,63)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(signs.shape[0], activation='softmax'))
+res = [.7, 0.2, 0.1]
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=200, callbacks=[tb_callback])
+model.summary()
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save('model.h5')
